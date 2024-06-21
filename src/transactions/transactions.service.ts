c9d3e5379ac89ede2309d528fd4344c445d2d348
Leonid Shaydenko <103574243+lssleo@users.noreply.github.com>
2024-06-22 01:55:40 +0300

    tx service optimization

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common'
-import { Contract, EventLog, ethers } from 'ethers'
+import { Contract, ethers } from 'ethers'
 import { ConfigService } from '@nestjs/config'
 import { WalletsService } from '../wallets/wallets.service'
 import { BalancesService } from '../balances/balances.service'
@@ -11,14 +11,10 @@ import { OnEvent } from '@nestjs/event-emitter'
 import { ProviderService } from 'src/providers/providers.service'
 import { NetworksService } from 'src/networks/networks.service'
 
-interface TokenWithContract extends token {
-    contract: Contract
-}
-
 @Injectable()
 export class TransactionsService {
     private wallets: { [address: string]: wallet } = {}
-    private tokens: { [id: number]: TokenWithContract } = {}
+    private tokens: { [key: string]: { token: token; contract: Contract } } = {}
     private networks: { [name: string]: network } = {}
 
     constructor(
@@ -34,18 +30,15 @@ export class TransactionsService {
     async initialize() {
         const walletsArray = await this.walletsService.findAll()
         walletsArray.forEach((wallet) => {
-            this.wallets[wallet.address] = wallet
+            this.wallets[wallet.address.toLowerCase()] = wallet
         })
         const tokensArray = await this.tokensService.findAllTokens()
         tokensArray.forEach((token) => {
+            const key = `${token.address.toLowerCase()}_${token.network.toLowerCase()}`
             const provider = this.providerService.getProvider(token.network.toLowerCase())
-            if (provider) {
-                this.tokens[token.id] = {
-                    ...token,
-                    contract: new ethers.Contract(token.address, erc20Abi, provider),
-                }
-            } else {
-                console.error(`Provider for network ${token.network} not found`)
+            this.tokens[key] = {
+                token: token,
+                contract: new ethers.Contract(token.address, erc20Abi, provider),
             }
         })
 
@@ -58,26 +51,28 @@ export class TransactionsService {
 
     @OnEvent('wallet.added')
     async handleWalletAdded(wallet: wallet) {
-        this.wallets[wallet.address] = wallet
+        this.wallets[wallet.address.toLowerCase()] = wallet
     }
 
     @OnEvent('wallet.removed')
     async handleWalletRemoved(wallet: wallet) {
-        delete this.wallets[wallet.address]
+        delete this.wallets[wallet.address.toLowerCase()]
     }
 
     @OnEvent('token.added')
     async handleTokenAdded(token: token) {
+        const key = `${token.address.toLowerCase()}_${token.network.toLowerCase()}`
         const provider = this.providerService.getProvider(token.network.toLowerCase())
-        if (provider) {
-            this.tokens[token.id] = {
-                ...token,
-                contract: new ethers.Contract(token.address, erc20Abi, provider),
-            }
-            await this.addTokenListeners(this.tokens[token.id])
-        } else {
-            console.error(`Provider for network ${token.network} not found`)
+        this.tokens[key] = {
+            token: token,
+            contract: new ethers.Contract(token.address, erc20Abi, provider),
         }
+    }
+
+    @OnEvent('token.removed')
+    async handleTokenRemoved(token: token) {
+        const key = `${token.address.toLowerCase()}_${token.network.toLowerCase()}`
+        delete this.tokens[key]
     }
 
     private async startListening() {
@@ -91,22 +86,22 @@ export class TransactionsService {
 
             provider.on('block', async (blockNumber) => {
                 const block = await provider.getBlock(blockNumber, true)
+                const logs = await provider.getLogs({
+                    blockHash: block.hash,
+                    topics: [ethers.id('Transfer(address,address,uint256)')],
+                })
+                logs.forEach(async (log) => {
+                    const key = `${log.address.toLowerCase()}_${networkEntity.name.toLowerCase()}`
+                    if (this.tokens[key]) {
+                        const from = `0x${log.topics[1].slice(26)}`.toLowerCase()
+                        const to = `0x${log.topics[2].slice(26)}`.toLowerCase()
+                        this.handleTokenTransfer(networkEntity.id, key, from, to)
+                    }
+                })
+
                 for (const transaction of block.prefetchedTransactions) {
                     await this.handleTransaction(networkEntity, transaction)
                 }
-            })
-        }
-        for (const tokenId in this.tokens) {
-            const token = this.tokens[tokenId]
-            token.contract.on(token.contract.filters.Transfer(), async (log: EventLog) => {
-                const addressFrom = log.args[0]
-                const addressTo = log.args[1]
-                await this.handleTokenTransfer(
-                    this.networks[token.network],
-                    token.id,
-                    addressFrom,
-                    addressTo,
-                )
             })
         }
     }
@@ -155,34 +150,21 @@ export class TransactionsService {
         }
     }
 
-    async handleTokenTransfer(network: network, tokenId: number, from: string, to: string) {
+    async handleTokenTransfer(networkId: number, key: string, from: string, to: string) {
         if (this.wallets[from] || this.wallets[to]) {
             const walletAddress = this.wallets[from] ? from : to
             const wallet = this.wallets[walletAddress]
 
-            const token = this.tokens[tokenId]
+            const token = this.tokens[key].token
 
-            const tokenBalance = await token.contract.balanceOf(wallet.address)
+            const tokenBalance = await this.tokens[key].contract.balanceOf(wallet.address)
 
             await this.balancesService.updateBalance(
                 wallet.id,
-                network.id,
+                networkId,
                 token.symbol,
                 ethers.formatUnits(tokenBalance, token.decimals),
             )
         }
-    }
-
-    async addTokenListeners(token: TokenWithContract) {
-        token.contract.on(token.contract.filters.Transfer(), async (log: EventLog) => {
-            const addressFrom = log.args[0]
-            const addressTo = log.args[1]
-            await this.handleTokenTransfer(
-                this.networks[token.network],
-                token.id,
-                addressFrom,
-                addressTo,
-            )
-        })
     }
 }
